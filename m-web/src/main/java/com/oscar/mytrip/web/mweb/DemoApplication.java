aa672b13b4591dabe4e42a0c4f14aeba9a0eb1cb
DhilipKumar <dhilip.dina@gmail.com>
2019-12-12 21:54:24 -0600

    Initial Version for web

---
+++ m-web/src/main/java/com/oscar/mytrip/web/mweb/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.oscar.mytrip.web.mweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
