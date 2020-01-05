0d6597e0e33590f088e0678ac5ec5e464e694c40
DhilipKumar <dhilip.dina@gmail.com>
2020-01-04 19:26:56 -0600

    Updated

--- zipkin-app-server/src/main/java/com/oscar/mytrip/zipkinserver/zipkinappserver/ZipkinAppServerApplication.java
+++ zipkin-app-server/src/main/java/com/oscar/mytrip/zipkinserver/zipkinappserver/ZipkinAppServerApplication.java
@@ -3,6 +3,9 @@ package com.oscar.mytrip.zipkinserver.zipkinappserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinAppServerApplication {
 
